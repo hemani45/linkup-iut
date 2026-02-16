e5e1e89f8d751b1471ee4516491f8701b3fd76e5
hemani45 <dzeugouaf@gmail.com>
2026-02-16 05:50:45 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,12 +4,17 @@ const firebaseConfig = {
   apiKey: "AIzaSyDgX33JA663R8Pyvfkww3mvep1n8_d0AxA",
   authDomain: "linkup-iut.firebaseapp.com",
   projectId: "linkup-iut",
- storageBucket: "linkup-iut.appspot.com"
+  storageBucket: "linkup-iut.appspot.com",
   messagingSenderId: "187321401718",
   appId: "1:187321401718:web:3a6effbcb0a6477b56ba57",
   measurementId: "G-Z8HV42713H"
 };
 
-// Initialisation Firebase (version classique)
+// Initialisation Firebase
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app();
+}
 
-firebase.initializeApp(firebaseConfig);
+console.log("✅ Firebase initialisé avec succès");
